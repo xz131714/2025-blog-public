0d6f0fc085ea4159b3cfe59fa9f02629eb9487a9
小张ฅ <zwl2247103169@gmail.com>
2025-12-05 23:06:28 +0800

    更新 consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -7,7 +7,7 @@ export const CARD_SPACING_SM = 24
  * GitHub 仓库配置
  */
 export const GITHUB_CONFIG = {
-	OWNER: process.env.NEXT_PUBLIC_GITHUB_OWNER || 'yysuni',
+	OWNER: process.env.NEXT_PUBLIC_GITHUB_OWNER || 'xz131714',
 	REPO: process.env.NEXT_PUBLIC_GITHUB_REPO || '2025-blog-public',
 	BRANCH: process.env.NEXT_PUBLIC_GITHUB_BRANCH || 'main',
 	APP_ID: process.env.NEXT_PUBLIC_GITHUB_APP_ID || '2409511'
